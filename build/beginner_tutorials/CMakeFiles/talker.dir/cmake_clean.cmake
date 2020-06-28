39e2adb149ee2158c165ee6055bf14557dd05730
DarrenSong <129114185@qq.com>
2020-06-28 21:33:15 +0800

    first commit

---
+++ build/beginner_tutorials/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/darren/projects/ros_study/catkin_ws/devel/lib/beginner_tutorials/talker"
+  "/home/darren/projects/ros_study/catkin_ws/devel/lib/beginner_tutorials/talker.pdb"
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/darren/projects/ros_study/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/darren/projects/ros_study/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/darren/projects/ros_study/catkin_ws/devel;/opt/ros/kinetic" != "" else []
